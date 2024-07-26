4bdede5d81a11159286ed8112695cf7c7dc7fc52
saru928 <kana.softball5@gmail.com>
2024-07-26 22:29:42 +0900

    2024/07/26

--- src/jp/co/diworks/action/Main.java
+++ src/jp/co/diworks/action/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World"); 
 		System.out.println(gokei());
-		System.out.println(gokei2(2,3));
+		System.out.println(gokei2(5,7));
+		System.out.println(circle(5));
 	}
 	
 	public static int gokei() {
@@ -14,5 +15,9 @@ public class Main {
 	
 	public static int gokei2(int number1, int number2) {
 		return number1 + number2;
+	}
+	
+	public static double circle(int hankei) {
+		return hankei*hankei*3.14;
 	}
 }
